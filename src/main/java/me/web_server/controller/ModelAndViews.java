5ebe28b1c6893ce15a0bea2f6fbaebc207f5bdc0
Dark-Legion <42568777+Dark-Legion@users.noreply.github.com>
2020-12-07 15:22:33 +0200

    Changed end-points to redirect to static files

--- src/main/java/me/web_server/controller/ModelAndViews.java
+++ src/main/java/me/web_server/controller/ModelAndViews.java
@@ -4,13 +4,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 class ModelAndViews {
+	final static ModelAndView ROOT = new ModelAndView("redirect:/index.html");
 	// final static ModelAndView ROOT_REDIRECT = new ModelAndView("redirect:/");
 	final static ModelAndView LOGIN_REDIRECT = new ModelAndView("redirect:/login");
 	final static ModelAndView LOGIN = new ModelAndView("login");
-	final static ModelAndView LOGIN_TYPE_SELECT = new ModelAndView("loginTypeSelect");
+	final static ModelAndView LOGIN_SELECT = new ModelAndView("redirect:/loginSelect.html");
 	final static ModelAndView MAIN_REDIRECT = new ModelAndView("redirect:/main");
 	final static ModelAndView ADMIN_MAIN = new ModelAndView("adminMain");
-	// final static ModelAndView SELLER_MAIN = new ModelAndView("sellerMain");
-	final static ModelAndView INVALID_LOGIN = new ModelAndView("invalidLogin", HttpStatus.NOT_ACCEPTABLE);
-	final static ModelAndView INTERNAL_ERROR = new ModelAndView("internalError", HttpStatus.INTERNAL_SERVER_ERROR);
+	final static ModelAndView SELLER_MAIN = new ModelAndView("sellerMain");
+	final static ModelAndView INVALID_LOGIN = new ModelAndView("redirect:/invalidLogin.html", HttpStatus.NOT_ACCEPTABLE);
+	final static ModelAndView INTERNAL_ERROR = new ModelAndView("redirect:/internalError.html", HttpStatus.INTERNAL_SERVER_ERROR);
 }
--- src/main/java/me/web_server/controller/WebController.java
+++ src/main/java/me/web_server/controller/WebController.java
@@ -6,126 +6,179 @@ import java.security.NoSuchAlgorithmException;
 import java.util.Optional;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
 import me.web_server.service.IUserService;
 
 @Controller
-@SessionAttributes("credentials")
 public class WebController {
 	@Autowired
 	private IUserService userService;
 
-	private boolean nonNullParameters(Object object) {
-		return object != null;
+	private final static String ADMIN_ATTRIBUTE = "admin";
+	private final static String USERNAME_ATTRIBUTE = "user";
+	private final static String PASSWORD_HASH_ATTRIBUTE = "pass";
+
+	private interface ModelAndVewCallable {
+		abstract ModelAndView get(String username, byte[] passwordHash);
 	}
 
-	private boolean nonNullParameters(Object object, Object... objects) {
-		return nonNullParameters(object) && nonNullParameters(objects);
+	private boolean nonNullParameters(Object... objects) {
+		if (objects == null) {
+			return false;
+		}
+		
+		for (Object object : objects) {
+			if (object == null) {
+				return false;
+			}
+		}
+
+		return true;
 	}
 
-	@ModelAttribute("credentials")
-	public Credentials newUserCredentials() {
-		return new Credentials();
-	}
+	public ModelAndView authenticateAndCallHandler(HttpSession session, HttpServletRequest request, ModelAndVewCallable adminPage, ModelAndVewCallable sellerPage, boolean loginRedirect) {
+		if (!AntiSessionHijack.validateSession(session, request)) {
+			return ModelAndViews.INVALID_LOGIN;
+		}
 
-	@GetMapping("/")
-	public ModelAndView rootGet(HttpServletRequest request) {
-		Boolean admin = Boolean.class.cast(request.getAttribute("admin"));
-		String username = String.class.cast(request.getAttribute("user"));
-		byte[] passwordHash = byte[].class.cast(request.getAttribute("pass"));
+		Boolean admin = getAdmin(session);
+		String username = getUsername(session);
+		byte[] passwordHash = getPasswordHash(session);
 
 		if (nonNullParameters(admin, username, passwordHash)) {
 			if (admin) {
 				if (userService.authenticateAdmin(username, passwordHash)) {
-					return ModelAndViews.MAIN_REDIRECT;
+					return adminPage.get(username, passwordHash);
 				} else {
 					return ModelAndViews.INVALID_LOGIN;
 				}
 			} else {
 				if (userService.authenticateSeller(username, passwordHash)) {
-					return ModelAndViews.MAIN_REDIRECT;
+					return sellerPage.get(username, passwordHash);
 				} else {
 					return ModelAndViews.INVALID_LOGIN;
 				}
 			}
 		}
 
-		return ModelAndViews.LOGIN_REDIRECT;
+		return (loginRedirect ? ModelAndViews.LOGIN_REDIRECT : ModelAndViews.INVALID_LOGIN);
 	}
 
-	private static void saveCredentials(@ModelAttribute("credentials") Credentials credentials, boolean admin, String username, byte[] passwordHash) {
-		credentials.setAdmin(Boolean.valueOf(admin));
-		credentials.setUsername(username);
-		credentials.setPasswordHash(passwordHash);
+	@GetMapping("/")
+	public ModelAndView rootGet() {
+		return ModelAndViews.ROOT;
+	}
+
+	private static Boolean getAdmin(HttpSession session) {
+		return Boolean.class.cast(session.getAttribute(ADMIN_ATTRIBUTE));
+	}
+
+	private static void setAdmin(HttpSession session, boolean admin) {
+		session.setAttribute(ADMIN_ATTRIBUTE, Boolean.valueOf(admin));
+	}
+
+	private static void setAdmin(HttpSession session, Boolean admin) {
+		session.setAttribute(ADMIN_ATTRIBUTE, admin);
+	}
+
+	private static String getUsername(HttpSession session) {
+		return String.class.cast(session.getAttribute(USERNAME_ATTRIBUTE));
+	}
+
+	private static void setUsername(HttpSession session, String username) {
+		session.setAttribute(USERNAME_ATTRIBUTE, username);
+	}
+
+	private static byte[] getPasswordHash(HttpSession session) {
+		return byte[].class.cast(session.getAttribute(PASSWORD_HASH_ATTRIBUTE));
+	}
+
+	private static void setPasswordHash(HttpSession session, byte[] passwordHash) {
+		session.setAttribute(PASSWORD_HASH_ATTRIBUTE, passwordHash);
+	}
+
+	private static void saveCredentials(HttpSession session, Boolean admin, String username, byte[] passwordHash) {
+		setAdmin(session, admin);
+		setUsername(session, username);
+		setPasswordHash(session, passwordHash);
 	}
 
     @GetMapping("/login")
     public ModelAndView loginGet() {
-        return ModelAndViews.LOGIN_TYPE_SELECT;
+        return ModelAndViews.LOGIN_SELECT;
     }
 
 	@PostMapping("/login")
 	public ModelAndView loginPost(
-		@ModelAttribute("credentials") Credentials credentials,
-		@RequestParam("loginType") Optional<String> loginTypeOption,
-		@RequestParam("user") Optional<String> username,
-		@RequestParam("pass") Optional<String> password
+		HttpSession session,
+		HttpServletRequest request,
+		@RequestParam("loginType") Optional<String> loginTypeOptional,
+		@RequestParam("user") Optional<String> usernameOptional,
+		@RequestParam("pass") Optional<String> passwordOptional
 	) {
-		Boolean admin = credentials.getAdmin();
+		Boolean admin = getAdmin(session);
 
-		if (loginTypeOption.isPresent()) {
-			String loginType = loginTypeOption.get();
+		if (loginTypeOptional.isPresent()) {
+			String loginType = loginTypeOptional.get();
 
 			if (loginType.equals("admin") || loginType.equals("seller")) {
-				credentials.setAdmin(Boolean.valueOf(loginType.equals("admin")));
+				setAdmin(session, loginType.equals("admin"));
 
 				return ModelAndViews.LOGIN;
 			} else {
 				return ModelAndViews.INVALID_LOGIN;
 			}
-		} else if (nonNullParameters(admin) && username.isPresent() && password.isPresent()) {
+		} else if (nonNullParameters(admin) && usernameOptional.isPresent() && passwordOptional.isPresent()) {
+			String username = usernameOptional.get();
+			String password = passwordOptional.get();
+
 			try {
 				byte[] passwordHash;
 
 				{
 					final MessageDigest digest = MessageDigest.getInstance("SHA3-256");
-					passwordHash = digest.digest(password.get().getBytes(StandardCharsets.UTF_8));
+					passwordHash = digest.digest(password.getBytes(StandardCharsets.UTF_8));
 				}
+
+				boolean authenticationSuccessful = false;
 
 				if (admin) {
-					if (userService.authenticateAdmin(username.get(), passwordHash)) {
-						saveCredentials(credentials, true, username.get(), passwordHash);
-
-						return ModelAndViews.MAIN_REDIRECT;
-					}
+					authenticationSuccessful = userService.authenticateAdmin(username, passwordHash);
 				} else {
-					if (userService.authenticateSeller(username.get(), passwordHash)) {
-						saveCredentials(credentials, false, username.get(), passwordHash);
-
-						return ModelAndViews.MAIN_REDIRECT;
-					}
+					authenticationSuccessful = userService.authenticateSeller(username, passwordHash);
 				}
 
-				return ModelAndViews.INVALID_LOGIN;
+				if (authenticationSuccessful) {
+					saveCredentials(session, admin, username, passwordHash);
+
+					return ModelAndViews.MAIN_REDIRECT;
+				} else {
+					return ModelAndViews.INVALID_LOGIN;
+				}
 			} catch (NoSuchAlgorithmException exception) {
 				return ModelAndViews.INTERNAL_ERROR;
 			}
 		} else {
-			return ModelAndViews.LOGIN_TYPE_SELECT;
+			return ModelAndViews.LOGIN_SELECT;
 		}
 	}
 
 	@GetMapping("/main")
-	public ModelAndView mainGet(HttpServletRequest request) {
-		return ModelAndViews.ADMIN_MAIN;
+	public ModelAndView mainGet(HttpSession session, HttpServletRequest request) {
+		return authenticateAndCallHandler(
+			session,
+			request,
+			(user, pass) -> { return ModelAndViews.ADMIN_MAIN; },
+			(user, pass) -> { return ModelAndViews.SELLER_MAIN; },
+			true
+		);
 	}
 }
